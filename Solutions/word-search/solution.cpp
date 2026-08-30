8616f3f52f9c663a8179331c753163cff62a5760
Shivam Chauhan <134799816+Shiv0-6@users.noreply.github.com>
2026-08-31 01:20:44 +0530

    Add Solutions/word-search/solution.cpp

---
+++ Solutions/word-search/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {class Solution {
+public:public:
+    bool exist(vector<vector<char>>& board,     bool exist(vector<vector<char>>& board, 
+    string word) {    string word) {
+        int m = board.size();        int m = board.size();
+        int n = board[0].size();        int n = board[0].size();
+                
+        // Start DFS from every cell in the         // Start DFS from every cell in the 
+        grid        grid
+        for (int i = 0; i < m; ++i) {        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {            for (int j = 0; j < n; ++j) {
+                // If the first character                 // If the first character 
+                matches, attempt to find the                 matches, attempt to find the 
+                full word                full word
+                if (dfs(board, word, i, j,                 if (dfs(board, word, i, j, 
+                0)) {                0)) {
+                    return true;                    return true;
+                }                }
+            }            }
+        }        }
+        return false;        return false;
