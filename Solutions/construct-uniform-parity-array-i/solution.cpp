2ba836851e7fd83d28ba0856163bcdcc92d21e65
Shivam Chauhan <134799816+Shiv0-6@users.noreply.github.com>
2026-09-03 03:49:04 +0530

    Add Solutions/construct-uniform-parity-array-i/solution.cpp

---
+++ Solutions/construct-uniform-parity-array-i/solution.cpp
@@ -0,0 +1,6 @@
+class Solution {class Solution {
+public:public:
+    bool uniformArray(vector<int>& nums1) {    bool uniformArray(vector<int>& nums1) {
+        return true;        return true;
+    }    }
+};};
