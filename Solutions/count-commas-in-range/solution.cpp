516e9e6535bf08edcea01ceb0e07e89b7b3f6785
Shivam Chauhan <134799816+Shiv0-6@users.noreply.github.com>
2026-09-08 23:42:15 +0530

    Add Solutions/count-commas-in-range/solution.cpp

---
+++ Solutions/count-commas-in-range/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {class Solution {
+public:public:
+    int countCommas(int n) {    int countCommas(int n) {
+        return max(0,n-999);        return max(0,n-999);
+              
+    }    }
+};};
