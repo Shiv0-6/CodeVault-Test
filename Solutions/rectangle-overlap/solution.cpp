767027bf2ef07dfac84374b298ca86431c68041b
Shivam Chauhan <134799816+Shiv0-6@users.noreply.github.com>
2026-09-15 01:04:22 +0530

    Add Solutions/rectangle-overlap/solution.cpp

---
+++ Solutions/rectangle-overlap/solution.cpp
@@ -0,0 +1,9 @@
+        bool yOverlap = rec1[1] < rec2[3] && rec2[1] < rec1        bool yOverlap = rec1[1] < rec2[3] && rec2[1] < rec1
+        [3];        [3];
+        [2];        [2];
+    rec2) {    rec2) {
+        // rec = [x1, y1, x2, y2]        // rec = [x1, y1, x2, y2]
+        bool xOverlap = rec1[0] < rec2[2] && rec2[0] < rec1        bool xOverlap = rec1[0] < rec2[2] && rec2[0] < rec1
+public:public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>&     bool isRectangleOverlap(vector<int>& rec1, vector<int>& 
+class Solution {class Solution {
